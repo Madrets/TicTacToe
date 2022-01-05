1c6b5cd83bbecb83bdd3ab0b48f69e05354a674c
Madrets <48129146+Madrets@users.noreply.github.com>
2022-01-05 17:38:53 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+/**
+ *
+ * The Main class for TicTacToe.java
+ *
+ * @author Emmanuel Elite
+ * @version January 5, 2022
+ *
+ *
+ */
+
 public class Main {
     public static void main(String[] args) {
         TicTacToe game1 = new TicTacToe();
         game1.initialize();
     }
 }
+
